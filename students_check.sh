493cf67bbde147f537f6f93e37dccf648118806f
blululu <6daolang@gmail.com>
2021-11-19 20:29:17 +0800

    basic success

--- students_check.sh
+++ students_check.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 dir=$1
-homework=$2
 
 model=/tmp/model.txt
 now=/tmp/now.txt
 
-# the ordinary files only contain id and name,like 1041180623-邓星宇;
+# the ordinary files only contain name,like 邓星宇;
 ls $1>$now
 cp ./model.txt $model
-sed 's/*-//' $model
-sed 's/;//' $model
+
+Name=`cat $model`
+n=0
+for i in $Name;do
+	cat $now|grep $i>/tmp/temp.txt
+	if [ ! -s /tmp/temp.txt ];then
+		echo "$i"
+		n=$(($n+1))
+	fi
+done
+
+Num_theory=`ls -l $model|wc -l`
+Num_exact=`ls -l $1|wc -l`
+if [ $(($Num_exact+$n)) -ne $Num_theory ];then
+	echo "some files named after wrongly or  duplicated files exists"
+fi
 
 
-ls>now.txt
-diff model.txt now.txt
