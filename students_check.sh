9036079fd337a87fe86133dc485a4b39011da56c
blululu <6daolang@gmail.com>
2021-11-19 20:09:34 +0800

    new method

--- students_check.sh
+++ students_check.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+dir=$1
+homework=$2
+
+model=/tmp/model.txt
+now=/tmp/now.txt
+
+# the ordinary files only contain id and name,like 1041180623-邓星宇;
+ls $1>$now
+cp ./model.txt $model
+sed 's/*-//' $model
+sed 's/;//' $model
+
+
 ls>now.txt
 diff model.txt now.txt
